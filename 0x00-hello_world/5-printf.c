b76568d39fa88ccd1735a5411fe62f02b7aaaef8
ilyasselhiba <ilyasseelhiba98@gmail.com>
2023-03-16 08:00:27 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - Prints out a string to stdout.
- * Return : 0 (Succes)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
