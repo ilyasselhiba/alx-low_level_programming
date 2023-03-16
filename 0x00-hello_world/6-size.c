064d25926e4c15d5ec8a5c68ba35b0f749e6ae3d
ilyasselhiba <ilyasseelhiba98@gmail.com>
2023-03-16 08:13:27 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,19 +2,18 @@
 /**
 * main - Entry point
 *
+* Description: Prints the size of various types on the computer
+*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(c));
-printf("Size of a clong long int: %lu byte(s) \n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+printf("Size of a double: %ld byte(s)\n", sizeof(double));
+printf("Size of a long double: %ld byte(s)\n", sizeof(long double));
 return (0);
 }
