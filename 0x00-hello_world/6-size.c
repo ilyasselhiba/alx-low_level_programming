d69d04e79ec71d1d74a0ec11dc37cf50d3ec982a
ilyasselhiba <ilyasseelhiba98@gmail.com>
2023-03-16 07:38:02 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,1 @@
-#!/bin/bash
 #include <stdio.h> int main() { printf("char:%zu,short:%zu,int:%zu,long:%zu,long long:%zu,float:%zu,double:%zu,long double:%zu",sizeof(char),sizeof(short),sizeof(int),sizeof(long),sizeof(long long),sizeof(float),sizeof(double),sizeof(long double)); return 0; }
