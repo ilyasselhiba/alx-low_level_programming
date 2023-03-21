7ed3a44ab36919b420a965bf6026bbf22c6fab66
ilyasselhiba <ilyasseelhiba98@gmail.com>
2023-03-21 07:26:33 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = '_';
+c = _putchar(c);
+c = 'p';
+c = _putchar(c);
+c = 'u';
+c = _putchar(c);
+c = 't';
+c = _putchar(c);
+c = 'c';
+c = _putchar(c);
+c = 'h';
+c = _putchar(c);
+c = 'a';
+c = _putchar(c);
+c = 'r';
+c = _putchar(c);
+c = '\n';
+c = _putchar(c);
+return (0);
+}
