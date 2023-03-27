cb564ebf5c3d357613c7f26b9f7be2f9ac429b8a
ilyasselhiba <ilyasseelhiba98@gmail.com>
2023-03-27 09:02:41 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
-#include <string.h>
-void print_rev(char *s);
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
 {
-int length = strlen(s);
-for (int i = length - 1; i >= 0; i--)
+int longi = 0;
+int o;
+while (*s != '\0')
 {
-printf("%c", s[i]);
+longi++;
+s++;
 }
-printf("\n");
+s--;
+for (o = longi; o > 0; o--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
 }
